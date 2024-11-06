197f7fa902d4e097794a7ac2bdb99f579dcd5a3b
EdineiJPJ <edineijosepozzajunior9@gmail.com>
2024-11-05 22:24:35 -0300

    Inserção item.js

--- Front-End/paginas/Item/item.js
+++ Front-End/paginas/Item/item.js
@@ -8,25 +8,27 @@ function adicionarItem() {
     if (nomeEpi == "" || ca == "" || quantidade == "" || tipoEpi == "") {
         alert("Por favor, preencha todos os campos.");
         return;
+
+    } else {
+        
+        const novaLinha = tabela.insertRow();
+        const celulaNome = novaLinha.insertCell(0);
+        const celulaCa = novaLinha.insertCell(1)
+        const celulaQuantidade = novaLinha.insertCell(2);
+        const celulaCategoria = novaLinha.insertCell(3);
+        const celulaAcao = novaLinha.insertCell(4);
+    
+        celulaNome.innerText = nomeEpi;
+        celulaCa = ca;
+        celulaQuantidade.innerText = quantidade;
+        celulaCategoria.innerText = tipoEpi;
+        celulaAcao.innerHTML = '<td class="text-center" style="width: 200px;"><button type ="submit " class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onclick="excluirLinha()"><i class="bi bi-trash-fill"></i>Excluir</button><button class="btn btn-primary"><i class="bi bi-pencil-square"></i>Editar</button></td>';
+    
+        document.getElementById("nomeItem").value = "";
+        document.getElementById("numeroCa").value = "";
+        document.getElementById("quantidadeItem").value = "";
+        document.getElementById("tipoItem").value = "";
     }
-
-    const novaLinha = tabela.insertRow();
-    const celulaNome = novaLinha.insertCell(0);
-    const celulaCa = novaLinha.insertCell(1)
-    const celulaQuantidade = novaLinha.insertCell(2);
-    const celulaCategoria = novaLinha.insertCell(3);
-    const celulaAcao = novaLinha.insertCell(4);
-
-    celulaNome.innerText = nomeEpi;
-    celulaCa = ca;
-    celulaQuantidade.innerText = quantidade;
-    celulaCategoria.innerText = tipoEpi;
-    celulaAcao.innerHTML = '<button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><i class="bi bi-trash-fill"></i>Excluir</button>';
-
-    document.getElementById("nomeItem").value = "";
-    document.getElementById("numeroCa").value = "";
-    document.getElementById("quantidadeItem").value = "";
-    document.getElementById("tipoItem").value = "";
 }
 
 function excluirLinha(botao) {
